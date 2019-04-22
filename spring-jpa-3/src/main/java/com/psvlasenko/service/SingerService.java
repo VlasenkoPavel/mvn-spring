cc2df4754fa9d56eccd30b1db8e4f3a1ff98f578
VlasenkoPavel <psvlasenko@gmail.com>
2019-04-22 09:07:03 +0300

    adde spring-jpa-3

---
+++ spring-jpa-3/src/main/java/com/psvlasenko/service/SingerService.java
@@ -0,0 +1,14 @@
+package com.psvlasenko.service;
+
+import com.psvlasenko.entities.Singer;
+
+import java.util.List;
+
+public interface SingerService {
+    List<Singer> findAll();
+    List<Singer> findAllWithAlbum();
+    Singer findById(Long id);
+    Singer save(Singer singer);
+    void delete(Singer singer);
+    List<Singer> findAllByNativeQuery();
+}
---
+++ spring-jpa-3/src/main/java/com/psvlasenko/service/SingerSummaryService.java
@@ -0,0 +1,9 @@
+package com.psvlasenko.service;
+
+import com.psvlasenko.view.SingerSummary;
+
+import java.util.List;
+
+public interface SingerSummaryService {
+    List<SingerSummary> findAll();
+}
